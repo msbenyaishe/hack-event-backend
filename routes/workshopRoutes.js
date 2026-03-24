c3ed99056c54089fe1d141a464df9e22c60a3eb9
Mohamed Said <mohamedsaidbenyaiche@gmail.com>
2026-03-24 17:59:40 +0100

    Fix

--- routes/workshopRoutes.js
+++ routes/workshopRoutes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/authMiddleware");
 const isAdminOrLeader = require("../middleware/isAdminOrLeader");
-const uploadPdf = require("../middleware/uploadPdf");
 
 const {
   createWorkshop,
